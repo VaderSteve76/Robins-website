e17f4967524658a17077b37fc217a8f6c62f09f5
VaderSteve76 <alverson.s@yahoo.com>
2022-01-03 20:07:25 -0500

    Typo

--- doodle-dreams/src/Img/Images.js
+++ doodle-dreams/src/Img/Images.js
@@ -4,9 +4,9 @@ const images = [
   { id: 3, src: './IMG_3.JPG', title: 'dog3', description: 'dog' },
   { id: 4, src: './IMG_4.JPG', title: 'dog4', description: 'dog' },
   { id: 5, src: './IMG_5.JPG', title: 'dog5', description: 'dog' },
-  { id: 5, src: './IMG_6.JPG', title: 'dog6', description: 'dog' },
-  { id: 5, src: './IMG_7.JPG', title: 'dog7', description: 'dog' },
-  { id: 5, src: './IMG_8.JPG', title: 'dog8', description: 'dog' }
+  { id: 6, src: './IMG_6.JPG', title: 'dog6', description: 'dog' },
+  { id: 7, src: './IMG_7.JPG', title: 'dog7', description: 'dog' },
+  { id: 8, src: './IMG_8.JPG', title: 'dog8', description: 'dog' }
 ];
 
 export default images;
