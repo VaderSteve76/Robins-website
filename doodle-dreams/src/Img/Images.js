10139919ae75de4dceb43c61fbbb2f3dedd3c48a
VaderSteve76 <alverson.s@yahoo.com>
2022-01-03 20:06:54 -0500

    Changed image names & exporting images for later use

---
+++ doodle-dreams/src/Img/Images.js
@@ -0,0 +1,12 @@
+const images = [
+  { id: 1, src: './IMG_1.JPG', title: 'dog1', description: 'dog' },
+  { id: 2, src: './IMG_2.JPG', title: 'dog2', description: 'dog' },
+  { id: 3, src: './IMG_3.JPG', title: 'dog3', description: 'dog' },
+  { id: 4, src: './IMG_4.JPG', title: 'dog4', description: 'dog' },
+  { id: 5, src: './IMG_5.JPG', title: 'dog5', description: 'dog' },
+  { id: 5, src: './IMG_6.JPG', title: 'dog6', description: 'dog' },
+  { id: 5, src: './IMG_7.JPG', title: 'dog7', description: 'dog' },
+  { id: 5, src: './IMG_8.JPG', title: 'dog8', description: 'dog' }
+];
+
+export default images;
